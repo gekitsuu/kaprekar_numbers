7a95a61f635c4b3976551c94ad72afcef232d6b6
Adam 'Gekitsuu' Glenn <gekitsuu@gmail.com>
2011-06-10 22:33:18 -0500

    Inital Import of this script

---
+++ kapnumbergen.py
@@ -0,0 +1,15 @@
+def next_num(primer):
+    number = primer
+    while True:
+        number += 1
+        yield number
+
+def solve_kap(num):
+    sqrdnum = str(num ** 2)
+    for i in range(1, len(sqrdnum)):
+        first, second = int(sqrdnum[:i]), int(sqrdnum[i:])
+        if (first + second) == num:
+            print num, sqrdnum, "(%s, %s)" % (first, second)
+
+for x in next_num(1):
+    solve_kap(x)
